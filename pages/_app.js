b6c9da69b471d3ab6a601d45400ce528ebbd77ca
kusupon <kusupon0628@gmail.com>
2023-01-10 23:01:46 +0900

    Vercel Analyticsを追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import { Meta } from '../components/meta/Seo'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Meta />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
